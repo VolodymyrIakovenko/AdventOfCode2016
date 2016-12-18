0391b69fc0b689e4c3390c06d4be6e13c0871d64
VolodymyrIakovenko <vayak38@mail.ru>
2016-12-18 14:56:05 +0100

    Added Day 18 solution.

---
+++ AdventOfCodeDay18/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new List<string>();
+
+            var startingMap = "^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^";
+            map.Add(startingMap);
+
+            var safeTiles = startingMap.Count(t => t.Equals('.'));
+
+            for (int i = 1; i < 400000; i++)
+            {
+                var newRow = string.Empty;
+                var lastRow = map[map.Count - 1];
+
+                for (var j = 0; j < lastRow.Length; j++)
+                {
+                    var leftIsTrap = j - 1 >= 0 && lastRow[j - 1] == '^';
+                    var centerIsTrap = lastRow[j] == '^';
+                    var righIsTrap = j + 1 < lastRow.Length && lastRow[j + 1] == '^';
+
+                    if ((leftIsTrap && centerIsTrap && !righIsTrap) ||
+                        (!leftIsTrap && centerIsTrap && righIsTrap) ||
+                        (leftIsTrap && !centerIsTrap && !righIsTrap) ||
+                        (!leftIsTrap && !centerIsTrap && righIsTrap))
+                    {
+                        newRow += '^';
+                    }
+                    else
+                    {
+                        newRow += '.';
+                        safeTiles++;
+                    }
+                }
+
+                map.Add(newRow);
+            }
+
+            foreach (var row in map)
+            {
+                Console.WriteLine(row);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Safe tiles: " + safeTiles);
+
+            Console.ReadKey();
+        }
+    }
+}
