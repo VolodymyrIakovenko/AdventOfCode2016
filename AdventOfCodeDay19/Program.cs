3d4e29ccc5fa93d9f86bcd97b73c433d37ec59e1
Volodymyr Iakovenko <volodymyr.iakovenko@tobii.com>
2016-12-19 16:42:06 +0100

    Added Day 19 solution.

---
+++ AdventOfCodeDay19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCodeDay19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elvesAmount = 3017957;
+            var elves = new List<int>();
+
+            for (int i = 1; i <= elvesAmount; i++)
+            {
+                elves.Add(i);
+            }
+
+            while (elves.Count > 1)
+            {
+                for (int i = 0; i < elves.Count; i++)
+                {
+                    var nextElfOffset = elves.Count /2;
+                    var nextElf = i + nextElfOffset;
+
+                    if (i + nextElfOffset >= elves.Count)
+                    {
+                        nextElf = nextElf - elves.Count;
+                        i--;
+                    }
+
+                    elves.RemoveAt(nextElf);
+                }
+            }
+
+            Console.WriteLine(elves[0]);
+            Console.ReadKey();
+        }
+    }
+}
