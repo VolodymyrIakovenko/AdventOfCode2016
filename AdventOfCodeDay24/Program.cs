1406257cb38658d6e3fdb9a10295caa35d5f8659
VolodymyrIakovenko <vayak38@mail.ru>
2017-01-08 21:09:08 +0100

    Added Day 24 and Day 25 solutions.

---
+++ AdventOfCodeDay24/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCodeDay24
+{
+    public class Position
+    {
+        public int X;
+        public int Y;
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return "[" + X + "," + Y + "]";
+        }
+
+        public string GetHashString()
+        {
+            return "|" + X + "," + Y + "|";
+        }
+    }
+
+    public class State
+    {
+        public Position CurrentPosition;
+        public string NotVisitedPositions;
+        public int Steps;
+
+        public State(Position currentPosition, string notVisitedPositions, int steps)
+        {
+            CurrentPosition = currentPosition;
+            NotVisitedPositions = notVisitedPositions;
+            Steps = steps;
+        }
+
+        public bool IsSuccess()
+        {
+            return NotVisitedPositions == string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return CurrentPosition + "; " + NotVisitedPositions;
+        }
+    }
+
+    class Program
+    {
+        public static int rows;
+        public static int columns;
+        public static Dictionary<string, string> visitedPositions = new Dictionary<string, string>();
+
+        public static void Main(string[] args)
+        {
+            var text = System.IO.File.ReadAllText(@"..\..\..\Input\Day24.txt");
+            var inputArray = text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            rows = inputArray.Length;
+            columns = inputArray[0].Length;
+
+            var startPosition = new Position(0, 0);
+            var notVisitedPositions = string.Empty;
+
+            var map = new char[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (inputArray[i][j] == '0')
+                    {
+                        startPosition = new Position(i, j);
+                    }
+                    else if (inputArray[i][j] != '#' && inputArray[i][j] != '.')
+                    {
+                        notVisitedPositions += (new Position(i, j)).GetHashString();
+                    }
+
+                    map[i, j] = inputArray[i][j];
+                }
+            }
+
+            // Output(map);
+
+            var currentState = new State(startPosition, notVisitedPositions, 0);
+            var stateAgenda = new Queue<State>();
+            stateAgenda.Enqueue(currentState);
+
+            var stepsCounter = 0;
+
+            while (stateAgenda.Count != 0)
+            {
+                currentState = stateAgenda.Dequeue();
+                var currentPositionHash = currentState.CurrentPosition.GetHashString();
+
+                if (visitedPositions.ContainsKey(currentState.NotVisitedPositions))
+                {
+                    if (visitedPositions[currentState.NotVisitedPositions].Contains(currentPositionHash))
+                    {
+                        continue;
+                    }
+
+                    visitedPositions[currentState.NotVisitedPositions] += currentPositionHash;
+                }
+                else
+                {
+                    visitedPositions.Add(currentState.NotVisitedPositions, currentPositionHash);
+                }
+
+                if (currentState.Steps > stepsCounter)
+                {
+                    stepsCounter++;
+                    //Console.WriteLine(stepsCounter);
+                }
+
+                if (currentState.IsSuccess())
+                {
+                    visitedPositions.Remove(startPosition.GetHashString());
+                    visitedPositions.Remove("");
+                    var stepsToStart = GetStepsToTheStart(map, currentState, startPosition);
+                    if (stepsToStart != -1)
+                    {
+                        Console.WriteLine("!!! " + (currentState.Steps + stepsToStart));
+                    }
+
+                    continue;
+                }
+
+                var nextPositions = GetNextPositions(map, currentState.CurrentPosition, false);
+                foreach (var nextPosition in nextPositions)
+                {
+                    var newNotVisitedPositions = currentState.NotVisitedPositions.Replace(nextPosition.GetHashString(), "");
+                    stateAgenda.Enqueue(new State(nextPosition, newNotVisitedPositions, currentState.Steps + 1));
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int GetStepsToTheStart(char[,] map, State state, Position startPosition)
+        {
+            var currentState = new State(state.CurrentPosition, startPosition.GetHashString(), 0);
+            var stateAgenda = new Queue<State>();
+            stateAgenda.Enqueue(currentState);
+
+            while (stateAgenda.Count != 0)
+            {
+                currentState = stateAgenda.Dequeue();
+                var currentPositionHash = currentState.CurrentPosition.GetHashString();
+
+                if (visitedPositions.ContainsKey(currentState.NotVisitedPositions))
+                {
+                    if (visitedPositions[currentState.NotVisitedPositions].Contains(currentPositionHash))
+                    {
+                        continue;
+                    }
+
+                    visitedPositions[currentState.NotVisitedPositions] += currentPositionHash;
+                }
+                else
+                {
+                    visitedPositions.Add(currentState.NotVisitedPositions, currentPositionHash);
+                }
+
+                if (currentState.IsSuccess())
+                {
+                    return currentState.Steps;
+                }
+
+                var nextPositions = GetNextPositions(map, currentState.CurrentPosition, false);
+                foreach (var nextPosition in nextPositions)
+                {
+                    var newNotVisitedPositions = currentState.NotVisitedPositions.Replace(nextPosition.GetHashString(), "");
+                    stateAgenda.Enqueue(new State(nextPosition, newNotVisitedPositions, currentState.Steps + 1));
+                }
+            }
+
+            return -1;
+        }
+
+        public static List<Position> GetNextPositions(char[,] map, Position position, bool filterStartPosition)
+        {
+            var nextPositions = new List<Position>();
+
+            var possiblePosition = new Position(position.X + 1, position.Y);
+            if (IsValidPosition(map, possiblePosition, filterStartPosition))
+            {
+                nextPositions.Add(possiblePosition);
+            }
+
+            possiblePosition = new Position(position.X, position.Y + 1);
+            if (IsValidPosition(map, possiblePosition, filterStartPosition))
+            {
+                nextPositions.Add(possiblePosition);
+            }
+
+            possiblePosition = new Position(position.X - 1, position.Y);
+            if (IsValidPosition(map, possiblePosition, filterStartPosition))
+            {
+                nextPositions.Add(possiblePosition);
+            }
+
+            possiblePosition = new Position(position.X, position.Y - 1);
+            if (IsValidPosition(map, possiblePosition, filterStartPosition))
+            {
+                nextPositions.Add(possiblePosition);
+            }
+
+            return nextPositions;
+        }
+
+        public static bool IsValidPosition(char[,] map, Position possiblePosition, bool filterStartPosition)
+        {
+            if (!filterStartPosition)
+            {
+                return possiblePosition.X >= 0 && possiblePosition.X < rows && possiblePosition.Y >= 0 && possiblePosition.Y < columns &&
+                    map[possiblePosition.X, possiblePosition.Y] != '#';
+            }
+            else
+            {
+                return possiblePosition.X >= 0 && possiblePosition.X < rows && possiblePosition.Y >= 0 && possiblePosition.Y < columns &&
+                map[possiblePosition.X, possiblePosition.Y] != '#' && map[possiblePosition.X, possiblePosition.Y] != '0';
+            }
+        }
+
+        public static void Output(char[,] map)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ AdventOfCodeDay25/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCodeDay25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            while (true)
+            {
+                Console.WriteLine(counter);
+
+                var registers = new Dictionary<char, int> { { 'a', counter } };
+
+                var text = System.IO.File.ReadAllText(@"..\..\..\Input\Day25.txt");
+                var inputArray = text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var output = new List<int>();
+
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    //Console.WriteLine(inputArray[i]);
+
+                    if (inputArray[i].StartsWith("cpy"))
+                    {
+                        var command = inputArray[i].Replace("cpy ", "");
+                        var copyValues = command.Split(' ');
+
+                        var copyValue = 0;
+                        var res = int.TryParse(copyValues[0], out copyValue);
+                        if (!res)
+                        {
+                            copyValue = registers[copyValues[0][0]];
+                        }
+
+                        var registerToCopy = copyValues[1][0];
+
+                        if (registers.ContainsKey(registerToCopy))
+                        {
+                            registers[registerToCopy] = copyValue;
+                        }
+                        else
+                        {
+                            registers.Add(registerToCopy, copyValue);
+                        }
+                    }
+                    else if (inputArray[i].StartsWith("inc"))
+                    {
+                        var command = inputArray[i].Replace("inc ", "");
+                        var registerToIncrease = command[command.Length - 1];
+
+                        if (registers.ContainsKey(registerToIncrease))
+                        {
+                            registers[registerToIncrease]++;
+                        }
+                    }
+                    else if (inputArray[i].StartsWith("dec"))
+                    {
+                        var command = inputArray[i].Replace("dec ", "");
+                        var registerToDecrease = command[command.Length - 1];
+
+                        if (registers.ContainsKey(registerToDecrease))
+                        {
+                            registers[registerToDecrease]--;
+                        }
+
+                        if (registerToDecrease == 'd')
+                        {
+                            Console.WriteLine(registers[registerToDecrease]);
+                        }
+                    }
+                    else if (inputArray[i].StartsWith("jnz"))
+                    {
+                        var command = inputArray[i].Replace("jnz ", "");
+                        var jumpValues = command.Split(' ');
+
+                        var registerValue1 = 0;
+                        var res1 = int.TryParse(jumpValues[1], out registerValue1);
+                        if (!res1)
+                        {
+                            var c = jumpValues[1][0];
+                            if (registers.ContainsKey(c))
+                            {
+                                registerValue1 = registers[c];
+                            }
+                        }
+
+                        var registerValue2 = 0;
+                        var res2 = int.TryParse(jumpValues[0], out registerValue2);
+                        if (!res2)
+                        {
+                            var c = jumpValues[0][0];
+                            if (registers.ContainsKey(c))
+                            {
+                                registerValue2 = registers[c];
+                            }
+                        }
+
+                        if (registerValue2 != 0)
+                        {
+                            i += registerValue1 - 1;
+                        }
+                    }
+                    else if (inputArray[i].StartsWith("tgl"))
+                    {
+                        var command = inputArray[i].Replace("jnz ", "");
+                        var tglValue = command[command.Length - 1];
+
+                        var offset = 0;
+                        var res = int.TryParse(tglValue.ToString(), out offset);
+                        if (!res)
+                        {
+                            offset = registers[tglValue];
+                        }
+
+                        if (i + offset >= inputArray.Length)
+                        {
+                            continue;
+                        }
+
+                        var splittedLine = inputArray[i + offset].Split(' ');
+                        var arguments = splittedLine.Length - 1;
+
+                        if (arguments == 1)
+                        {
+                            if (inputArray[i + offset].StartsWith("inc"))
+                            {
+                                inputArray[i + offset] = inputArray[i + offset].Replace("inc", "dec");
+                            }
+                            else
+                            {
+                                var firstWord = splittedLine[0];
+                                inputArray[i + offset] = inputArray[i + offset].Replace(firstWord, "inc");
+                            }
+                        }
+
+                        if (arguments == 2)
+                        {
+                            if (inputArray[i + offset].StartsWith("jnz"))
+                            {
+                                inputArray[i + offset] = inputArray[i + offset].Replace("jnz", "cpy");
+                            }
+                            else
+                            {
+                                var firstWord = splittedLine[0];
+                                inputArray[i + offset] = inputArray[i + offset].Replace(firstWord, "jnz");
+                            }
+                        }
+                    }
+                    else if (inputArray[i].StartsWith("out"))
+                    {
+                        var commandValue = inputArray[i].Replace("out ", "");
+
+                        var registerValue = 0;
+                        var res = int.TryParse(commandValue, out registerValue);
+                        if (!res)
+                        {
+                            var c = commandValue[0];
+                            if (registers.ContainsKey(c))
+                            {
+                                registerValue = registers[c];
+                            }
+                        }
+
+                        output.Add(registerValue);
+                        if (!IsValidOutput(output))
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                counter++;
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool IsValidOutput(List<int> output)
+        {
+            for (int i = 0; i < output.Count; i++)
+            {
+                if (i % 2 != output[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
